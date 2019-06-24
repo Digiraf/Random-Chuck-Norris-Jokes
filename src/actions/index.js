9c402cb80d54412cbf3ad6c12388f8e5d87acbfa
digiraf <digiraf@gmail.com>
2019-06-24 08:41:00 +0100

    menu bar & loading jokes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,25 @@
 import axios from 'axios';
 import Config from '../config/config'
-export function getJokes(max,scope){
+export function getJokes(max,scope,cb){
+  scope.props.isRegister(['loading',true]);
+  serverRequest("get",max,{},response => {
 
+          if(response.status&&response.res){
+            cb(response.res);
+          }
+
+
+  });
 }
 export  function isRegister(data){
     return {type:"IS_REGISTER_UPDATE", payload:data};
+}
+export function serverRequest(method,url,data,callback) {
+  const req = axios[method](Config.host+url, data);
+  req.then((response) => {
+    callback({status:1,res:response.data})
+  }).catch((error) => {
+    callback({status:0,error:error})
+  });
+
 }
--- src/home.js
+++ src/home.js
@@ -7,23 +7,96 @@ import {
 import {
   bindActionCreators
 } from 'redux';
-import {isRegister} from './actions/index';
+import {isRegister,getJokes} from './actions/index';
 class App extends Component {
   constructor(props) {
     super(props);
   }
   componentDidMount() {
       if(this.props.register&&!this.props.register.state){
-              this.props.isRegister(['state','store']);
-
+              this.props.isRegister(['state','remote']);
+                this.getRemoteList()
+      }
+  }
+  activeList(key){
+      if(this.props.register.state&&this.props.register.state===key){
+        return 'bttn active'
+      }else{
+        return 'bttn'
+      }
+  }
+  getRemoteJokesBttn(){
+    return (
+      <div className={this.activeList('remote')}>🏠</div>
+    )
+  }
+  getLocalJokesBttn(){
+    return (
+      <div className={this.activeList('local')}>🌟</div>
+    )
+  }
+  jokesBar(){
+    return (
+      <div className="jokesbar">
+        {this.getRemoteJokesBttn()}
+        {this.getLocalJokesBttn()}
+      </div>
+    )
+  }
+  getJoker(){
+    return(
+      <span className="emojijoker">😂</span>
+    )
+  }
+  loader(){
+      if(this.props.register.loading){
+        return(
+          <div className="loader">loading jokes{this.getJoker()}</div>
+        )
+      }else{
+        return null;
       }
 
   }
+  getRemoteList(){
+      var scope=this;
+
+      getJokes(10,this,(jokes)=>{
+
+          if(jokes&&jokes.type&&jokes.type==='success'){
+            console.log(jokes.value)
+            scope.props.isRegister(['jokes',jokes.value])
+          }
+            scope.props.isRegister(['loading',false]);
+      })
+
+  }
+  renderJokes(){
+        if(this.props.register.jokes){
+          console.log( this.props.register.jokes)
+          return this.props.register.jokes.map((joke)=>{
+
+              return (
+
+                  <div key={joke.id}>{joke.id}</div>
+              )
+          })
+        }else{
+          return null;
+        }
+
+    }
+
   render() {
     return (
-      <div>hola</div>
+      <div>
+          {this.jokesBar()}
+          <div className="jokesList">
+          {this.loader()}
+          {this.renderJokes()}
+          </div>
+      </div>
     )
-
   }
 }
 
